8195361ec13dd6b5544b2d830223891cb79046b1
bellapacx <bellapacx@gmail.com>
2025-09-09 22:22:15 +0300

    Initial commit: Telegram Bingo bot setupdsssssdrrL

--- config/db.go
+++ config/db.go
@@ -5,8 +5,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/bellapacxx/bingo-backend/models"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -22,12 +20,6 @@ func ConnectDB() {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("[FATAL] Failed to connect to database: %v", err)
-	}
-
-	// Auto migrate models
-	err = db.AutoMigrate(&models.User{}, &models.Game{}, &models.Card{}, &models.Transaction{})
-	if err != nil {
-		log.Fatalf("[FATAL] AutoMigrate failed: %v", err)
 	}
 
 	DB = db
